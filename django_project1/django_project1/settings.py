173d3b1a154689419605d29348e92b4022662c84
DavidCatrina <david08catrina@gmail.com>
2023-02-01 20:06:40 +0000

    add register functionality

--- django_project1/django_project1/settings.py
+++ django_project1/django_project1/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'crispy_forms',
     'users.apps.UsersConfig',
     'blog.apps.BlogConfig',
     'django.contrib.admin',
@@ -123,3 +124,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
+CRISPY_TEMPLATE_PACK = "bootstrap4"
--- django_project1/users/views.py
+++ django_project1/users/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import UserRegisterForm
 
-# Create your views here.
+
+def register(request):
+    if request.method == "POST":
+        #creates a post with the form's data
+        form = UserRegisterForm(request.POST) 
+
+        # check if the data is valid
+        if form.is_valid(): 
+
+            # save the user to the database
+            form.save() 
+            username = form.cleaned_data.get('username') # get data in good format
+
+            #show a success message
+            messages.success(request, f'Account created for {username}!')
+
+            #redirects to the blog page using the name value of the blog url patterns
+            return redirect('blog-home')
+
+    else:
+        #creates an empty form
+        form = UserRegisterForm() 
+
+    
+    context = {
+        'form': form
+    }
+    return render(request, 'users/register.html', context)
